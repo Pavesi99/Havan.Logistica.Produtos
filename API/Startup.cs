56f84bc085f6e0390e27d4548b9513d90bb417bc
Vilmar Pavesi Junior <juniorpavesi999@gmail.com>
2022-05-03 22:01:10 -0300

    adicionar routeprefix para swagger

--- API/Startup.cs
+++ API/Startup.cs
@@ -53,7 +53,10 @@ namespace API
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+                app.UseSwaggerUI(c => {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+                    c.RoutePrefix = "";
+                });
             }
 
             app.UseHttpsRedirection();
